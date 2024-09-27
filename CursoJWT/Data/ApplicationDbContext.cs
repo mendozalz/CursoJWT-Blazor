a91c07fd2ed76bd66872516dc73fbbb08a770f09
Flavio <flavioanez@gmail.com>
2024-09-27 00:21:30 -0500

    Agregar archivos de proyecto.

---
+++ CursoJWT/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CursoJWT.Shared;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace CursoJWT.Server.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+		}
+
+
+		public DbSet<Usuario> Usuarios { get; set; }
+	}
+}
---
+++ CursoJWT/Program.cs
@@ -0,0 +1,72 @@
+using CursoJWT.Client.Pages;
+using CursoJWT.Components;
+using CursoJWT.Server.Data;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+builder.Services.AddAuthorizationCore();//Agregado
+builder.Services.AddScoped<AuthenticationStateProvider, ServerAuthenticationStateProvider>();//Agregado
+builder.Services.AddControllers();//agregado
+builder.Services.AddHttpClient();//Agregado
+builder.Services.AddCascadingAuthenticationState();//Agregado
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+}
+
+);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+	.AddJwtBearer(options =>
+	{
+		options.TokenValidationParameters = new TokenValidationParameters
+		{
+			ValidateIssuerSigningKey = true,
+			IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
+				.GetBytes("PROYECTO CONTROL USUARUIS EN BLAZOR WEB WASM_ DAGO PARA BLAZOR  PARA APPS")),
+			ValidateIssuer = false,
+			ValidateAudience = false
+		};
+	});//agregado
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+app.MapControllers();//Agregado
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(CursoJWT.Client._Imports).Assembly);
+
+app.UseAuthentication(); // Asegúrate de agregar el middleware de autenticación
+app.UseAuthorization();  // Asegúrate de agregar el middleware de autorización
+
+app.Run();
